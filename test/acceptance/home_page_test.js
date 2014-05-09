900c21df4249a2fb9d91ac19b3b14a79c5e26a44
Nick Harberg <nick@seamonsterstudios.com>
2014-05-09 13:30:03 -0700

    httpstatus message changed

--- test/acceptance/home_page_test.js
+++ test/acceptance/home_page_test.js
@@ -4,7 +4,7 @@
 casper.test.begin('home page', 3, function suite(test) {
 
   casper.start('http://localhost:3000/', function() {
-    test.assertHttpStatus(200, 'The App is Alive!');
+    test.assertHttpStatus(200, 'The App is Alive! Run for your lives!');
   });
 
   casper.then(function(){
